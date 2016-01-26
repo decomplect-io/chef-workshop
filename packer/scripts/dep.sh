35a64dff514e5dc4153af221ddd770e23a001de9
Sahil Muthoo <sahil.muthoo@gmail.com>
2016-01-26 16:29:47 +0530

    Setup Chef DK

--- packer/scripts/dep.sh
+++ packer/scripts/dep.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 #
 # Setup the the box. This runs as root
+set -e
 
 apt-get -y update
 
-apt-get -y install curl
+apt-get -y install curl axel
 
 # You can install anything you need here.
+axel 'https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_0.10.0-1_amd64.deb'
+dpkg -i chefdk*
+rm chefdk*
